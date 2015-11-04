c71bfe4087bd12b4501a4a48a8c846f9d9b61c1d
Justin Walrath <walrathjaw@gmail.com>
2015-11-04 00:40:21 -0600

    Added user signin, added session, finish user card design.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,20 +15,6 @@ import walrath.technology.openwalrus.model.tos.Grunt
 import walrath.technology.openwalrus.model.tos.GruntTO
 
 class Application @Inject() (userManager: UserManager) extends Controller {
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
-  def read(handle:String, password:String) = Action {
-    userManager.login(handle, password) match {
-      case Some(x) => Ok("pass")
-      case None => Ok("fail")
-    }
-  }
-  
-  def signup = Action {
-    Ok(views.html.createuser("English" :: Nil))
-  }
   
   val signupForm = Form(
     tuple(
@@ -39,15 +25,47 @@ class Application @Inject() (userManager: UserManager) extends Controller {
     )
   )
   
+  val signinForm = Form(
+    tuple(
+      "handle" -> nonEmptyText,
+      "password" -> nonEmptyText
+    )    
+  )
+  
+  def index = Action { implicit request =>
+    request.session.get("userHandle").map { handle =>
+      val result = userManager.getUserProfile(handle)
+      result match {
+        case (Some(x), _) => Ok(views.html.profile(result._1.get, result._2.getOrElse(List.empty)))
+        case _ => Redirect(routes.Application.loadLogin)
+      }
+    }.getOrElse {
+      Ok(views.html.index())
+    }
+  }
+
+  def read(handle:String, password:String) = Action {
+    userManager.login(handle, password) match {
+      case Some(x) => Ok("pass")
+      case None => Ok("fail")
+    }
+  }
+  
+  def signup = Action { implicit request =>
+    request.session.get("userHandle").map { handle =>
+      Redirect(routes.Application.index)
+    }.getOrElse {
+      Ok(views.html.createuser("English" :: Nil))
+    }
+  }
+  
   def performSignup = Action { implicit request =>
     val (fullName, handle, phoneoremail, password) = signupForm.bindFromRequest.get
     
     if(!userManager.checkIfHandleInUse(handle)) {
       val newUser = User(None, handle, enterEmail(phoneoremail), convertToDomesticPhone(phoneoremail), password, fullName, 0, true, false)
       val result = userManager.createUser(newUser)
-      Ok(views.html.profile(newUser, List.empty[GruntTO]))
-      //Ok(userManager.createUser(User(None, handle, enterEmail(phoneoremail), convertToDomesticPhone(phoneoremail), password, fullName, 0, true, false)).toString())
-      
+      loginRedirect(newUser)
     }
     else {
       Ok("Handle in use")
@@ -62,5 +80,34 @@ class Application @Inject() (userManager: UserManager) extends Controller {
     }
   }
   
-  def enterEmail(email: String): Option[String] = if(checkIfPossiblyEmail(email)) Some(email) else None
+  def loadLogin = Action { implicit request =>
+    request.session.get("userHandle").map { handle =>
+      Redirect(routes.Application.index)
+    }.getOrElse {
+      Ok(views.html.login(None))
+    }
+  }
+  
+  def attemptSignin = Action { implicit request =>
+    val (handle, password) = signinForm.bindFromRequest.get
+    userManager.login(handle, password).map { user => 
+      loginRedirect(user)
+    }.getOrElse {
+      Ok(views.html.login(Some("Username and password did not match...")))
+    }
+  }
+  
+  def logout = Action {
+    Redirect(routes.Application.index()) withNewSession
+  }
+  
+  def followingPage(handle: String) = TODO
+  
+  def followersPage(handle: String) = TODO
+  
+  private def enterEmail(email: String): Option[String] = if(checkIfPossiblyEmail(email)) Some(email) else None
+
+  private def loginRedirect(user: User): Result = Redirect(routes.Application.index) withSession(
+    "userHandle" -> user.handle
+  )
 }
